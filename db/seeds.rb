21c9856d7c2e1d7e0336b1ff604dd27ac963e37e
Quentin Peschard <qpeschard49@gmail.com>
2021-08-11 16:48:16 +0200

    finished exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,89 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W6 2DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms,
+                open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Bohemian flat in Camden',
+  address: '10 Napoleon Street London D9 1DT',
+  description: 'A cute place for a couple with the tube station nearby',
+  price_per_night: 152,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Luxurious penthouse in central London',
+  address: 'Buckingham Palace London W9 5DT',
+  description: 'A huge location for family members and friends, the host lives here but is rarely at the Palace',
+  price_per_night: 942,
+  number_of_guests: 8
+)
+Flat.create!(
+  name: 'The cheapest closet in London',
+  address: '20 Cheap Bitch Ave London W9 9DT',
+  description: 'An affordable closet for youngsters with limited budget',
+  price_per_night: 20,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W6 2DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms,
+                open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Bohemian flat in Camden',
+  address: '10 Napoleon Street London D9 1DT',
+  description: 'A cute place for a couple with the tube station nearby',
+  price_per_night: 152,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Luxurious penthouse in central London',
+  address: 'Buckingham Palace London W9 5DT',
+  description: 'A huge location for family members and friends, the host lives here but is rarely at the Palace',
+  price_per_night: 942,
+  number_of_guests: 8
+)
+Flat.create!(
+  name: 'The cheapest closet in London',
+  address: '20 Cheap Bitch Ave London W9 9DT',
+  description: 'An affordable closet for youngsters with limited budget',
+  price_per_night: 20,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W6 2DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms,
+                open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Bohemian flat in Camden',
+  address: '10 Napoleon Street London D9 1DT',
+  description: 'A cute place for a couple with the tube station nearby',
+  price_per_night: 152,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Luxurious penthouse in central London',
+  address: 'Buckingham Palace London W9 5DT',
+  description: 'A huge location for family members and friends, the host lives here but is rarely at the Palace',
+  price_per_night: 942,
+  number_of_guests: 8
+)
+Flat.create!(
+  name: 'The cheapest closet in London',
+  address: '20 Cheap Bitch Ave London W9 9DT',
+  description: 'An affordable closet for youngsters with limited budget',
+  price_per_night: 20,
+  number_of_guests: 1
+)
